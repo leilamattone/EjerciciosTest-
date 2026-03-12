056e2910e60fc09d5c6f59736dc49afcf4ea3d84
leilamattone <leilamattone@sisorg.com>
2026-03-12 09:11:40 -0300

    feat: Add new Selenium test cases with Page Object Model implementation2

--- 01.Pages/LoginPage.cs
+++ 01.Pages/LoginPage.cs
@@ -1,10 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using OpenQA.Selenium;
 
-namespace EjerciciosTest._01.Pages
+namespace EjerciciosTest.Pages
 {
     internal class LoginPage
     {
+        // Campo privado para almacenar el driver
+        private readonly IWebDriver driver;
+
+        // Constructor que recibe el driver desde los tests
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void IngresarEmail(string email)
+        {
+            driver.FindElement(By.Id("input-email")).SendKeys(email);
+        }
+
+        public void IngresarPassword(string password)
+        {
+            driver.FindElement(By.Id("input-password")).SendKeys(password);
+        }
+
+        public void HacerClickLogin()
+        {
+            driver.FindElement(By.CssSelector("input[value='Login']")).Click();
+        }
+
+        public void HacerClickLogOut()
+        {
+            driver.FindElement(By.LinkText("Logout")).Click();
+        }
+
+        public void Login(string email, string password)
+        {
+            IngresarEmail(email);
+            IngresarPassword(password);
+            HacerClickLogin();
+        }
     }
 }
--- 03.Test/Test_Base.cs
+++ 03.Test/Test_Base.cs
@@ -1,9 +1,41 @@
-﻿namespace EjerciciosTest._03.Test
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace EjerciciosTest.Test
 {
     // [TestClass]
     public class Test_Base
     {
         // Aquí puedes agregar cualquier configuración común para tus pruebas, como inicialización de datos, configuración de entorno, etc.
         // Driver, TestInitialize, TestCleanup, etc.
+
+        // Cambiar a protected para que las clases hijas puedan acceder
+        protected IWebDriver? Driver;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            //Opciones de configuración para el navegador Chrome
+            var options = new ChromeOptions();
+            options.AddArgument("--disable-blink-features=AutomationControlled");
+            options.AddArgument("--start-maximized");
+            options.AddExcludedArgument("enable-automation");
+            options.AddAdditionalOption("useAutomationExtension", false);
+
+            // Ignorar errores de certificado SSL (solo para entornos de prueba)
+            options.AcceptInsecureCertificates = true;
+            options.AddArgument("--ignore-certificate-errors");
+            options.AddArgument("--ignore-ssl-errors");
+
+            //Inicialización del controlador de Chrome con las opciones configuradas
+            Driver = new ChromeDriver(options);
+        }
+
+        [TestCleanup]
+        //Cierre de conexión
+        public void TearDown()
+        {
+            Driver?.Quit();
+        }
     }
 }
